6c64438e471c104a0e232819dedb7fb8c9c297db
vikash <vikashraj144@gmail.com>
2019-07-06 09:45:46 +0530

    parse application/json and look for raw text added

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 let express = require('express');
 let app = express();
 let port = 8080;
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+
+//parse application/json and look for raw text                                        
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: 'application/json'
+}));
+//use morgan to log at command line
+app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+
 
 app.listen(port);
 console.log("Listening on port " + port);
